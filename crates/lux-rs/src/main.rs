0e3a474f769a4d278c17a46d97fe746f846d85a0
Shukhrat Mukimov <shukhrat.mukimov@auto1.com>
2026-08-23 20:35:09 +0200

    feat(lux-rs): add new crate for MQTT‑based illuminance daemon

---
+++ crates/lux-rs/src/main.rs
@@ -0,0 +1,120 @@
+use std::time::Duration;
+
+use log::{Level, info, warn};
+use rumqttc::{AsyncClient, Event, MqttOptions, Packet, QoS};
+use serde::Deserialize;
+use tokio::{
+    io::AsyncWriteExt,
+    net::UnixStream,
+    signal::unix::{SignalKind, signal},
+};
+
+// Systemd service file for this program:
+// Restart=on-failure
+// RestartSec=5s
+// EnvironmentFile=/etc/default/lux-rs
+// After=darkman.service
+// PartOf=graphical-session.target
+
+// Example message from zigbee2mqtt:
+// {"battery":80,"illuminance":5,"linkquality":160,"voltage":2800}
+#[derive(Deserialize)]
+struct Message {
+    illuminance: Option<u32>,
+}
+
+async fn send_command(socket_path: &str, command: &str) -> std::io::Result<()> {
+    let mut stream = UnixStream::connect(socket_path).await?;
+
+    stream.write_all(format!("{command}\n").as_bytes()).await?;
+
+    stream.shutdown().await
+}
+
+fn env(key: &str) -> Result<String, String> {
+    std::env::var(key).map_err(|e| format!("{key}: {e}"))
+}
+
+// observed max raw illuminance for this sensor
+const MAX_ILLUMINANCE: f64 = 157.0;
+const LIGHT_THRESHOLD: f64 = 0.28;
+const DARK_THRESHOLD: f64 = 0.22;
+const TOPIC: &str = "zigbee2mqtt/0xa4c138c7b629cbbd";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    simple_logger::init_with_level(Level::Info)?;
+    let runtime_dir = env("XDG_RUNTIME_DIR")?;
+    let ha_password = env("HA_PASSWORD")?;
+    let ha_user = env("HA_USERNAME")?;
+    let socket_path = format!("{runtime_dir}/darkman/control.sock");
+    let mut mqttoptions = MqttOptions::new("lux-rs", "homeassistant", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(60));
+    mqttoptions.set_credentials(ha_user, ha_password);
+
+    let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+    let mut sigint = signal(SignalKind::interrupt())?;
+    let mut sigterm = signal(SignalKind::terminate())?;
+    let mut cache_command: Option<&str> = None;
+    loop {
+        let event = tokio::select! {
+            _ = sigint.recv()=> {
+                info!("SIGINT received, shutting down");
+                break;
+            }
+            _ = sigterm.recv()=> {
+                info!("SIGTERM received, shutting down");
+                break;
+            }
+            event = eventloop.poll() => event,
+        };
+        match event {
+            Ok(event) => match event {
+                Event::Incoming(Packet::ConnAck(_)) => {
+                    if let Err(e) = client.subscribe(TOPIC, QoS::AtMostOnce).await {
+                        warn!("Error subscribing to topic: {e}");
+                    } else {
+                        info!("Subscribed to topic: {TOPIC}");
+                    }
+                }
+                Event::Incoming(Packet::Publish(publish)) => {
+                    match serde_json::from_slice::<Message>(publish.payload.as_ref()) {
+                        Ok(Message {
+                            illuminance: Some(lux),
+                        }) => {
+                            let illuminance = f64::from(lux) / MAX_ILLUMINANCE;
+                            info!("Illuminance = {:.2}%", illuminance * 100.0);
+
+                            let command = if illuminance < DARK_THRESHOLD {
+                                Some("set dark")
+                            } else if illuminance > LIGHT_THRESHOLD {
+                                Some("set light")
+                            } else {
+                                None
+                            };
+
+                            if let Some(command) = command
+                                && cache_command != Some(command)
+                            {
+                                match send_command(&socket_path, command).await {
+                                    Ok(()) => cache_command = Some(command),
+                                    Err(e) => {
+                                        warn!("darkman socket write failed: {e}");
+                                    }
+                                }
+                            }
+                        }
+                        Ok(_) => {}
+                        Err(e) => warn!("Could not parse payload: {e}"),
+                    }
+                }
+                _ => {}
+            },
+            Err(e) => {
+                warn!("{e}");
+            }
+        }
+    }
+
+    Ok(())
+}
